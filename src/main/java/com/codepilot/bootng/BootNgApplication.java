8220a0e34e9aa6f5e9568dd1832301b7409c0c04
Sai Krishna Vangala <skvangala@interac.ca>
2020-04-18 16:32:17 -0400

    first commit

---
+++ src/main/java/com/codepilot/bootng/BootNgApplication.java
@@ -0,0 +1,13 @@
+package com.codepilot.bootng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNgApplication.class, args);
+	}
+
+}
